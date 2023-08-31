3a7894f19ad2fff0c81ff12bf73b2c8296f5be96
emmanuelonwukwe <emmanuelonwukwe202@gmail.com>
2023-08-31 15:10:39 +0100

    Update the root search field to use data gotten from urlSearchParams and sync it to the form state

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,14 +7,20 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
-
 export async function action() {
   const contact = await createContact();
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
+}
+
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q} = useLoaderData();
   const navigation = useNavigation();
 
   return (
@@ -29,6 +35,7 @@ export default function Root() {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
@@ -75,9 +82,4 @@ export default function Root() {
       </div>
     </>
   );
-}
-
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
 }
